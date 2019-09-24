f8e6b3d7a3ca575cb5ed4a4de4f188578e476175
Prakash Somasundaram <prakash.somasundaram@proenco.com>
2019-09-24 12:24:59 +0530

    Migrated the code based to .net standard Fixed the hangfire api breaking changes Fixed the tests TODO: Yet to fix Appveyor build

--- src/Hangfire.SqlServer.RabbitMq/RabbitMqJobQueue.cs
+++ src/Hangfire.SqlServer.RabbitMq/RabbitMqJobQueue.cs
@@ -2,6 +2,10 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
+#if FEATURE_TRANSACTIONSCOPE
+#else
+using System.Data.Common;
+#endif
 using System.Text;
 using System.Threading;
 using Hangfire.Annotations;
@@ -12,118 +16,133 @@ using RabbitMQ.Client.Events;
 
 namespace Hangfire.SqlServer.RabbitMQ
 {
-    /// <summary>
-    /// An <see cref="IPersistentJobQueue"/> implementatation for RabbitMQ.
-    /// Maintains a single RabbitMQ <see cref="IConnection"/>, as well as a dedicated <see cref="IModel"/> (aka channel) for publishing and 
-    /// one for consuming messages. The consumer channel owns a <see cref="QueueingBasicConsumer"/> for each
-    /// queue configured on the parent <see cref="IPersistentJobQueueProvider"/>.
-    /// </summary>
-    public class RabbitMqJobQueue : IPersistentJobQueue, IDisposable
-    {
-        private static readonly int SyncReceiveTimeout = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
-        private static readonly object ConnectionLock = new object(); // used when re-creating the connection
-        private static readonly object ConsumerLock = new object();   // used for channel creation and serialzing ACK messages
-        private static readonly object PublisherLock = new object();  // used for channel creation and serializing Publish messages
-        private readonly IEnumerable<string> _queues;
-        private readonly ConnectionFactory _factory;
-        private readonly Action<IModel> _confConsumer;
-        private readonly ConcurrentDictionary<string, QueueingBasicConsumer> _consumers;
-        private IConnection _connection;
-        private IModel _consumerChannel;
-        private IModel _publisherChannel;
+	/// <summary>
+	/// An <see cref="IPersistentJobQueue"/> implementatation for RabbitMQ.
+	/// Maintains a single RabbitMQ <see cref="IConnection"/>, as well as a dedicated <see cref="IModel"/> (aka channel) for publishing and 
+	/// one for consuming messages. The consumer channel owns a <see cref="QueueingBasicConsumer"/> for each
+	/// queue configured on the parent <see cref="IPersistentJobQueueProvider"/>.
+	/// </summary>
+	public class RabbitMqJobQueue : IPersistentJobQueue, IDisposable
+	{
+		private static readonly int SyncReceiveTimeout = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
+		private static readonly object ConnectionLock = new object(); // used when re-creating the connection
+		private static readonly object ConsumerLock = new object();   // used for channel creation and serialzing ACK messages
+		private static readonly object PublisherLock = new object();  // used for channel creation and serializing Publish messages
+		private readonly IEnumerable<string> _queues;
+		private readonly ConnectionFactory _factory;
+		private readonly Action<IModel> _confConsumer;
+		private readonly ConcurrentDictionary<string, QueueingBasicConsumer> _consumers;
+		private IConnection _connection;
+		private IModel _consumerChannel;
+		private IModel _publisherChannel;
 
-        private static readonly Hangfire.Logging.ILog Logger = Hangfire.Logging.LogProvider.For<RabbitMqJobQueue>();
+		private static readonly Hangfire.Logging.ILog Logger = Hangfire.Logging.LogProvider.For<RabbitMqJobQueue>();
 
-        public RabbitMqJobQueue(IEnumerable<string> queues, ConnectionFactory factory,
-            [CanBeNull] Action<IModel> confConsumer = null)
-        {
-            if (queues == null) throw new ArgumentNullException("queues");
-            if (factory == null) throw new ArgumentNullException("factory");
+		public RabbitMqJobQueue(IEnumerable<string> queues, ConnectionFactory factory,
+			[CanBeNull] Action<IModel> confConsumer = null)
+		{
+			_queues = queues ?? throw new ArgumentNullException("queues");
+			_factory = factory ?? throw new ArgumentNullException("factory");
+			_confConsumer = confConsumer ?? (_ => { });
+			_connection = factory.CreateConnection();
+			_consumers = new ConcurrentDictionary<string, QueueingBasicConsumer>();
+		}
 
-            _queues = queues;
-            _factory = factory;
-            _confConsumer = confConsumer ?? (_ => {});
-            _connection = factory.CreateConnection();
-            _consumers = new ConcurrentDictionary<string, QueueingBasicConsumer>();
-        }
+		internal IModel Channel
+		{
+			get
+			{
+				EnsureConsumerChannel();
+				return _consumerChannel;
+			}
+		}
 
-        internal IModel Channel
-        {
-            get
-            {
-                EnsureConsumerChannel();
-                return _consumerChannel;
-            }
-        }
+		public IFetchedJob Dequeue(string[] queues, CancellationToken cancellationToken)
+		{
+			EnsureConsumerChannel();
 
-        public IFetchedJob Dequeue(string[] queues, CancellationToken cancellationToken)
-        {
-            EnsureConsumerChannel();
+			BasicDeliverEventArgs message;
+			var queueIndex = 0;
 
-            BasicDeliverEventArgs message;
-            var queueIndex = 0;
+			do
+			{
+				queueIndex = (queueIndex + 1) % queues.Length;
+				var queueName = queues[queueIndex];
 
-            do
-            {
-                queueIndex = (queueIndex + 1) % queues.Length;
-                var queueName = queues[queueIndex];
+				cancellationToken.ThrowIfCancellationRequested();
 
-                cancellationToken.ThrowIfCancellationRequested();
+				try
+				{
+					var consumer = GetConsumerForQueue(queueName, cancellationToken);
+					consumer.Queue.Dequeue(SyncReceiveTimeout, out message);
+				}
+				catch (global::RabbitMQ.Client.Exceptions.AlreadyClosedException)
+				{
+					EnsureConsumerChannel();
+					message = null;
+				}
+				catch (System.IO.EndOfStreamException)
+				{
+					EnsureConsumerChannel();
+					message = null;
+				}
 
-                try
-                {
-                    var consumer = GetConsumerForQueue(queueName, cancellationToken);
-                    consumer.Queue.Dequeue(SyncReceiveTimeout, out message);
-                }
-                catch (global::RabbitMQ.Client.Exceptions.AlreadyClosedException)
-                {
-                    EnsureConsumerChannel();
-                    message = null;
-                }
-                catch (System.IO.EndOfStreamException)
-                {
-                    EnsureConsumerChannel();
-                    message = null;
-                }
+			} while (message == null);
 
-            } while (message == null);
+			var jobId = Encoding.UTF8.GetString(message.Body);
+			var deliveryTag = message.DeliveryTag;
 
-            var jobId = Encoding.UTF8.GetString(message.Body);
-            var deliveryTag = message.DeliveryTag;
+			return new RabbitMqFetchedJob(jobId,
+				() => {
+					try
+					{
+						// not calling CreateChannel() as Ack/Nack need to be send on originating channel,
+						// instead logging possible exceptions here
+						lock (ConsumerLock)
+						{
+							_consumerChannel.BasicAck(deliveryTag, false);
+						}
+					}
+					catch (Exception ex)
+					{
+						Logger.ErrorException($"An error occurred sending basic.ack for Job#{jobId}.", ex);
+					}
+				},
+				() => {
+					try
+					{
+						lock (ConsumerLock)
+						{
+							_consumerChannel.BasicNack(deliveryTag, false, true);
+							_consumerChannel.Close(global::RabbitMQ.Client.Framing.Constants.ReplySuccess, "Requeue");
+						}
+					}
+					catch (Exception ex)
+					{
+						Logger.ErrorException($"An error occurred sending basic.nack for Job#{jobId}.", ex);
+					}
+				});
+		}
 
-            return new RabbitMqFetchedJob(jobId,
-                () => {
-                    try
-                    {
-                        // not calling CreateChannel() as Ack/Nack need to be send on originating channel,
-                        // instead logging possible exceptions here
-                        lock (ConsumerLock)
-                        {
-                            _consumerChannel.BasicAck(deliveryTag, false);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.ErrorException($"An error occurred sending basic.ack for Job#{jobId}.", ex);
-                    }
-                },
-                () => {
-                    try
-                    {
-                        lock (ConsumerLock)
-                        {
-                            _consumerChannel.BasicNack(deliveryTag, false, true);
-                            _consumerChannel.Close(global::RabbitMQ.Client.Framing.Constants.ReplySuccess, "Requeue");
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.ErrorException($"An error occurred sending basic.nack for Job#{jobId}.", ex);
-                    }
-                });
-        }
+#if FEATURE_TRANSACTIONSCOPE
+		public void Enqueue(IDbConnection connection, string queue, string jobId)
+		{
+			EnsurePublisherChannel();
 
-        public void Enqueue(IDbConnection connection, string queue, string jobId)
+			lock (PublisherLock) // Serializes all messages on publish channel
+			{
+				var body = Encoding.UTF8.GetBytes(jobId);
+				var properties = _publisherChannel.CreateBasicProperties();
+				properties.Persistent = true;
+
+				// TODO Allow to specify non-default exchange
+				_publisherChannel.BasicPublish("", queue, properties, body);
+
+				Logger.Debug($"Job enqueued: {jobId}");
+			}
+		}
+#else
+        public void Enqueue(DbConnection connection, DbTransaction transaction, string queue, string jobId)
         {
             EnsurePublisherChannel();
 
@@ -139,106 +158,107 @@ namespace Hangfire.SqlServer.RabbitMQ
                 Logger.Debug($"Job enqueued: {jobId}");
             }
         }
+#endif
 
-        public void Dispose()
-        {
-            if (_consumerChannel != null)
-            {
-                if (_consumerChannel.IsOpen) _consumerChannel.Close();
-                _consumerChannel.Dispose();
-                _consumerChannel = null;
-            }
+		public void Dispose()
+		{
+			if (_consumerChannel != null)
+			{
+				if (_consumerChannel.IsOpen) _consumerChannel.Close();
+				_consumerChannel.Dispose();
+				_consumerChannel = null;
+			}
 
-            if (_publisherChannel != null)
-            {
-                if (_publisherChannel.IsOpen) _publisherChannel.Close();
-                _publisherChannel.Dispose();
-                _publisherChannel = null;
-            }
+			if (_publisherChannel != null)
+			{
+				if (_publisherChannel.IsOpen) _publisherChannel.Close();
+				_publisherChannel.Dispose();
+				_publisherChannel = null;
+			}
 
-            if (_connection != null)
-            {
-                if (_connection.IsOpen) _connection.Close();
-                _connection.Dispose();
-                _connection = null;
-            }
-        }
+			if (_connection != null)
+			{
+				if (_connection.IsOpen) _connection.Close();
+				_connection.Dispose();
+				_connection = null;
+			}
+		}
 
-        private void EnsureConsumerChannel()
-        {
-            if (_consumerChannel != null && _consumerChannel.IsOpen && _connection.IsOpen) return;
+		private void EnsureConsumerChannel()
+		{
+			if (_consumerChannel != null && _consumerChannel.IsOpen && _connection.IsOpen) return;
 
-            lock (ConsumerLock)
-            {
-                CreateChannel(ref _consumerChannel);
-                _confConsumer(_consumerChannel);
-            }
-        }
+			lock (ConsumerLock)
+			{
+				CreateChannel(ref _consumerChannel);
+				_confConsumer(_consumerChannel);
+			}
+		}
 
-        private void EnsurePublisherChannel()
-        {
-            if (_publisherChannel != null && _publisherChannel.IsOpen && _connection.IsOpen) return;
+		private void EnsurePublisherChannel()
+		{
+			if (_publisherChannel != null && _publisherChannel.IsOpen && _connection.IsOpen) return;
 
-            lock (PublisherLock)
-            {
-                CreateChannel(ref _publisherChannel);
-            }
-        }
+			lock (PublisherLock)
+			{
+				CreateChannel(ref _publisherChannel);
+			}
+		}
 
-        private void CreateChannel(ref IModel channel)
-        {
-            if (channel != null && channel.IsOpen) channel.Abort();
-            if (!_connection.IsOpen)
-            {
-                lock (ConnectionLock)
-                    if (!_connection.IsOpen) _connection = _factory.CreateConnection();
-            }
-            channel = _connection.CreateModel();
+		private void CreateChannel(ref IModel channel)
+		{
+			if (channel != null && channel.IsOpen) channel.Abort();
+			if (!_connection.IsOpen)
+			{
+				lock (ConnectionLock)
+					if (!_connection.IsOpen) _connection = _factory.CreateConnection();
+			}
+			channel = _connection.CreateModel();
 
-            // QueueDeclare is idempotent
-            foreach (var queue in _queues)
-                channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false, arguments: null); // be aware that monitoring API also performs QueueDeclare
-        }
+			// QueueDeclare is idempotent
+			foreach (var queue in _queues)
+				channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false, arguments: null); // be aware that monitoring API also performs QueueDeclare
+		}
 
-        private QueueingBasicConsumer GetConsumerForQueue(string queue, CancellationToken cancellationToken)
-        {
-            QueueingBasicConsumer consumer;
+		private QueueingBasicConsumer GetConsumerForQueue(string queue, CancellationToken cancellationToken)
+		{
+			QueueingBasicConsumer consumer;
 
-            cancellationToken.ThrowIfCancellationRequested();
+			cancellationToken.ThrowIfCancellationRequested();
 
-            if(!_consumers.TryGetValue(queue, out consumer))
-            {
-                // Need to create a new consumer since a consumer for the queue does not exist
-                lock (ConsumerLock)
-                {
-                    if (!_consumers.TryGetValue(queue, out consumer))
-                    {
-                        consumer = new QueueingBasicConsumer(_consumerChannel);
-                        _consumers.AddOrUpdate(queue, consumer, (dq, dc) => consumer);
-                        _consumerChannel.BasicConsume(queue, false, "Hangfire.RabbitMq." + Thread.CurrentThread.Name, consumer);
-                    }
-                }
-            }
-            else
-            {
-                // Consumer for the queue exists, ensure that the channel (Model) is not closed
-                if (consumer.Model.IsClosed)
-                {
-                    lock (ConsumerLock)
-                    {
-                        if (consumer.Model.IsClosed)
-                        {
-                            // Recreate the consumer with the new channel
-                            var newConsumer = new QueueingBasicConsumer(_consumerChannel);
-                            _consumers.AddOrUpdate(queue, newConsumer, (dq, dc) => newConsumer);
-                            _consumerChannel.BasicConsume(queue, false, "Hangfire.RabbitMq." + Thread.CurrentThread.Name, newConsumer);
-                            consumer = newConsumer;
-                        }
-                    }
-                }
-            }
+			if (!_consumers.TryGetValue(queue, out consumer))
+			{
+				// Need to create a new consumer since a consumer for the queue does not exist
+				lock (ConsumerLock)
+				{
+					if (!_consumers.TryGetValue(queue, out consumer))
+					{
+						consumer = new QueueingBasicConsumer(_consumerChannel);
+						_consumers.AddOrUpdate(queue, consumer, (dq, dc) => consumer);
+						_consumerChannel.BasicConsume(queue, false, "Hangfire.RabbitMq." + Thread.CurrentThread.Name, consumer);
+					}
+				}
+			}
+			else
+			{
+				// Consumer for the queue exists, ensure that the channel (Model) is not closed
+				if (consumer.Model.IsClosed)
+				{
+					lock (ConsumerLock)
+					{
+						if (consumer.Model.IsClosed)
+						{
+							// Recreate the consumer with the new channel
+							var newConsumer = new QueueingBasicConsumer(_consumerChannel);
+							_consumers.AddOrUpdate(queue, newConsumer, (dq, dc) => newConsumer);
+							_consumerChannel.BasicConsume(queue, false, "Hangfire.RabbitMq." + Thread.CurrentThread.Name, newConsumer);
+							consumer = newConsumer;
+						}
+					}
+				}
+			}
 
-            return consumer;
-        }
-    }
+			return consumer;
+		}
+	}
 }
--- tests/Hangfire.SqlServer.RabbitMq.Tests/RabbitMqJobQueueFacts.cs
+++ tests/Hangfire.SqlServer.RabbitMq.Tests/RabbitMqJobQueueFacts.cs
@@ -7,167 +7,177 @@ using Xunit;
 
 namespace Hangfire.SqlServer.RabbitMq.Tests
 {
-    public class RabbitMqJobQueueFacts
-    {
-        private readonly CancellationToken _token;
+	public class RabbitMqJobQueueFacts
+	{
+		private readonly CancellationToken _token;
 
-        public RabbitMqJobQueueFacts()
-        {
-            _token = new CancellationTokenSource().Token;
-        }
+		public RabbitMqJobQueueFacts()
+		{
+			_token = new CancellationTokenSource().Token;
+		}
 
-        [Fact]
-        public void Ctor_ThrowsAnException_WhenQueueIsNull()
-        {
-            var exception = Assert.Throws<ArgumentNullException>(
-                () => new RabbitMqJobQueue(null, null, null));
+		[Fact]
+		public void Ctor_ThrowsAnException_WhenQueueIsNull()
+		{
+			var exception = Assert.Throws<ArgumentNullException>(
+				() => new RabbitMqJobQueue(null, null, null));
 
-            Assert.Equal("queues", exception.ParamName);
-        }
+			Assert.Equal("queues", exception.ParamName);
+		}
 
-        [Fact]
-        public void Ctor_ThrowsAnException_WhenFactoryIsNull()
-        {
-            var exception = Assert.Throws<ArgumentNullException>(
-                () => new RabbitMqJobQueue(new[]{"my-queue"}, null, null));
+		[Fact]
+		public void Ctor_ThrowsAnException_WhenFactoryIsNull()
+		{
+			var exception = Assert.Throws<ArgumentNullException>(
+				() => new RabbitMqJobQueue(new[] { "my-queue" }, null, null));
 
-            Assert.Equal("factory", exception.ParamName);
-        }
+			Assert.Equal("factory", exception.ParamName);
+		}
 
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void Enqueue_SendsTheJobId()
-        {
-            // Arrange
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                // Act
-                queue.Enqueue(new Mock<IDbConnection>().Object, "my-queue", "job-id");
-
-                // Assert
-                var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
-                fetchedJob.RemoveFromQueue();
-
-                var body = fetchedJob.JobId;
-
-                Assert.Equal("job-id", body);
-            }
-        }
-
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void Dequeue_ReturnsFetchedJob_WithJobId()
-        {
-            RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
-
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
-                fetchedJob.RemoveFromQueue();
-
-                Assert.Equal("job-id", fetchedJob.JobId);
-            }
-        }
-
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void Dequeue_ThrowsCanceledException_WhenTokenHasBeenCancelled()
-        {
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                var token = new CancellationToken(true);
-
-                Assert.Throws<OperationCanceledException>(
-                    () => queue.Dequeue(new[] {"my-queue"}, token));
-            }
-        }
-
-        [FactWithTimeout, CleanRabbitMqQueue("queue-1", "queue-2")]
-        public void Dequeue_ReturnsFetchedJob_FromOtherQueues_IfFirstAreEmpty()
-        {
-            RabbitMqUtils.EnqueueJobId("queue-2", "job-id");
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("queue-1", "queue-2"))
-            {
-                var fetchedJob = queue.Dequeue(new[] {"queue-1", "queue-2"}, _token);
-
-                Assert.Equal("job-id", fetchedJob.JobId);
-            }
-        }
-
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void Dequeue_MakesJobInvisibleForOtherFetchers()
-        {
-            // Arrange
-            RabbitMqUtils.EnqueueJobId("my-queue", "job-id9");
-
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                // Act
-                var fetchedJob = queue.Dequeue(new[] {"my-queue"}, _token);
-
-                // Assert
-                Assert.NotNull(fetchedJob);
-
-                var exception = Assert.Throws<TimeoutException>(
-                    () => RabbitMqUtils.DequeueJobId("my-queue", TimeSpan.FromSeconds(1)));
-
-                Assert.Equal(exception.Message, "my-queue");
-            }
-        }
-
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void RemoveFromQueue_OnFetchedJob_RemovesTheJobCompletely()
-        {
-            // Arrange
-            RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                // Act
-                using (var fetchedJob = queue.Dequeue(new[] {"my-queue"}, _token))
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void Enqueue_SendsTheJobId()
+		{
+			// Arrange
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				// Act
+#if FEATURE_TRANSACTIONSCOPE
+				var connection = new Mock<IDbConnection>().Object;
+				queue.Enqueue(connection, "my-queue", "job-id");
+#else
+                var connection = new Mock<System.Data.Common.DbConnection>().Object;
+                using (var transaction = connection.BeginTransaction())
                 {
-                    fetchedJob.RemoveFromQueue();
+                    queue.Enqueue(connection, transaction, "my-queue", "job-id");
+                    transaction.Commit();
                 }
+#endif
 
-                // Assert
-                var exception = Assert.Throws<TimeoutException>(
-                    () => RabbitMqUtils.DequeueJobId("my-queue", TimeSpan.FromSeconds(1)));
+				// Assert
+				var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+				fetchedJob.RemoveFromQueue();
 
-                Assert.Equal(exception.Message, "my-queue");
-            }
-        }
+				var body = fetchedJob.JobId;
 
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void DisposeWithoutRemoval_OnFetchedJob_ReturnsTheJobToTheQueue()
-        {
-            // Arrange
-            RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
+				Assert.Equal("job-id", body);
+			}
+		}
 
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                // Act
-                var fetchedJob = queue.Dequeue(new[] {"my-queue"}, _token);
-                fetchedJob.Dispose();
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void Dequeue_ReturnsFetchedJob_WithJobId()
+		{
+			RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
 
-                // Assert
-                var jobId = RabbitMqUtils.DequeueJobId("my-queue", TimeSpan.FromSeconds(5));
-                Assert.Equal("job-id", jobId);
-            }
-        }
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+				fetchedJob.RemoveFromQueue();
 
-        [FactWithTimeout, CleanRabbitMqQueue("my-queue")]
-        public void DisposeWithoutRemoval_OnFetchedJob_CanBeDequeuedAgain()
-        {
-            // Arrange
-            RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
+				Assert.Equal("job-id", fetchedJob.JobId);
+			}
+		}
 
-            using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
-            {
-                // Act
-                var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
-                fetchedJob.Dispose();
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void Dequeue_ThrowsCanceledException_WhenTokenHasBeenCancelled()
+		{
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				var token = new CancellationToken(true);
 
-                // Assert
-                var newFetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+				Assert.Throws<OperationCanceledException>(
+					() => queue.Dequeue(new[] { "my-queue" }, token));
+			}
+		}
 
-                Assert.Equal("job-id", newFetchedJob.JobId);
-            }
-        }
-    }
+		[FactWithTimeout, CleanRabbitMqQueue("queue-1", "queue-2")]
+		public void Dequeue_ReturnsFetchedJob_FromOtherQueues_IfFirstAreEmpty()
+		{
+			RabbitMqUtils.EnqueueJobId("queue-2", "job-id");
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("queue-1", "queue-2"))
+			{
+				var fetchedJob = queue.Dequeue(new[] { "queue-1", "queue-2" }, _token);
+
+				Assert.Equal("job-id", fetchedJob.JobId);
+			}
+		}
+
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void Dequeue_MakesJobInvisibleForOtherFetchers()
+		{
+			// Arrange
+			RabbitMqUtils.EnqueueJobId("my-queue", "job-id9");
+
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				// Act
+				var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+
+				// Assert
+				Assert.NotNull(fetchedJob);
+
+				var exception = Assert.Throws<TimeoutException>(
+					() => RabbitMqUtils.DequeueJobId("my-queue", TimeSpan.FromSeconds(1)));
+
+				Assert.Equal(exception.Message, "my-queue");
+			}
+		}
+
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void RemoveFromQueue_OnFetchedJob_RemovesTheJobCompletely()
+		{
+			// Arrange
+			RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				// Act
+				using (var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token))
+				{
+					fetchedJob.RemoveFromQueue();
+				}
+
+				// Assert
+				var exception = Assert.Throws<TimeoutException>(
+					() => RabbitMqUtils.DequeueJobId("my-queue", TimeSpan.FromSeconds(1)));
+
+				Assert.Equal(exception.Message, "my-queue");
+			}
+		}
+
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void DisposeWithoutRemoval_OnFetchedJob_ReturnsTheJobToTheQueue()
+		{
+			// Arrange
+			RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
+
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				// Act
+				var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+				fetchedJob.Dispose();
+
+				// Assert
+				var jobId = RabbitMqUtils.DequeueJobId("my-queue", TimeSpan.FromSeconds(5));
+				Assert.Equal("job-id", jobId);
+			}
+		}
+
+		[FactWithTimeout, CleanRabbitMqQueue("my-queue")]
+		public void DisposeWithoutRemoval_OnFetchedJob_CanBeDequeuedAgain()
+		{
+			// Arrange
+			RabbitMqUtils.EnqueueJobId("my-queue", "job-id");
+
+			using (var queue = CleanRabbitMqQueueAttribute.GetMessageQueue("my-queue"))
+			{
+				// Act
+				var fetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+				fetchedJob.Dispose();
+
+				// Assert
+				var newFetchedJob = queue.Dequeue(new[] { "my-queue" }, _token);
+
+				Assert.Equal("job-id", newFetchedJob.JobId);
+			}
+		}
+	}
 }
